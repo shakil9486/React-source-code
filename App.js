4332b4809bb1d39b97092a7d212b1aa5c7f7aa81
shakil9486 <shakil9486@gmail.com>
2020-07-24 21:52:03 +0600

    90. Component Creation Lifecycle in Action 7

--- App.js
+++ App.js
@@ -4,6 +4,10 @@ import Persons from '../components/Persons/Persons';
 import Cockpit from '../components/Cockpit/Cockpit';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    console.log('[App.js] constructor');
+  }
     state = {
       persons: [
         { id: 'asfa1',name: 'Max', age:28 },
@@ -12,6 +16,19 @@ class App extends Component {
       ],
       otherState: 'some other value',
       showPersons: false
+    }
+
+    static getDerivedStateFromProps(props, state) {
+      console.log('[App.js] getDerivedStateFromProps', props);
+      return state;
+    }
+
+    componentWillMount() {
+      console.log('[App.js] componentWillMount');
+    }
+
+    componentDidMount() {
+      console.log('[App.js] componentDidMount');
     }
 
  
@@ -47,8 +64,8 @@ class App extends Component {
   }
 
     render() {
+    console.log('[App.js] render');
     let persons = null;
-    let btnclass = '';
 
     if (this.state.showPersons) {
       persons = 
