f07fc8bade568e12a6f02821fd3654ead0ac5ed5
shakil9486 <shakil9486@gmail.com>
2020-07-27 03:44:22 +0600

    95. Cleaning up with Lifecycle Hooks & useEffect() 7

--- Cockpit.js
+++ Cockpit.js
@@ -9,7 +9,17 @@ const cockpit = (props) => {
         setTimeout(() => {
             alert('Saved date to cloud');
         }, 1000);
-    }, []); //[props.persons]
+        return () => {
+            console.log('[Cockpit.js] cleanup work in useEffect');
+        };
+    }, []);
+
+    useEffect(() => {
+        console.log('[Cockpit.js] 2nd useEffect');
+        return () => {
+            console.log('[Cockpit.js] cleanup work in 2nd useEffect');
+        };
+    });
 
     //useEffect();
 
