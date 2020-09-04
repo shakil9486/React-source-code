99bf16d34ec94c6ffc1e62bc449a52a0d9b5894a
shakil9486 <shakil9486@gmail.com>
2020-09-04 06:04:37 +0600

    106. Another Form of HOCs 7

--- App.js
+++ App.js
@@ -3,7 +3,8 @@ import React, { Component } from 'react';
 import classes from'./App.css';
 import Persons from '../components/Persons/Persons';
 import Cockpit from '../components/Cockpit/Cockpit';
-import WithClass from '../hoc/WithClass';
+import withClass from '../hoc/withClass';
+import Aux from '../hoc/Auxiliary';
 
 class App extends Component {
   constructor(props) {
@@ -88,7 +89,7 @@ class App extends Component {
     }
 
     return (
-        <WithClass classes={classes.App}>  
+        <Aux>  
           <button onClick={() => {
             this.setState({ showCockpit: false});
           }}>
@@ -103,9 +104,9 @@ class App extends Component {
             clicked={this.togglePersonsHandler} />
           ) : null}
           {persons}
-        </WithClass>
+        </Aux>
     );
   }
 }
 
-export default App;
+export default withClass(App, classes.App);
