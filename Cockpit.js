2afe5571273ec81cddfad5c73d8bb482f1d6bc98
shakil9486 <shakil9486@gmail.com>
2020-09-07 18:32:28 +0600

    112. Understanding Prop Chain Problems 7

--- Cockpit.js
+++ Cockpit.js
@@ -49,6 +49,7 @@ const cockpit = (props) => {
             onClick={props.clicked}>
                 Toggle Persons
             </button>
+            <button onClick={props.login}>Log in</button>
         </div>
     );
 };
